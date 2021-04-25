42e204b888b97499670175085860827c7830d865
Z0060883 <marcian.bondoc-popescu.external@trw.com>
2021-04-25 17:29:12 +0300

    first_commit

---
+++ build/precache-manifest.80c9a416abf70059c9092a8e81472f2a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8f5fcf4cbfde98311d6af0d1d766c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283140cf90b87553dbe0",
+    "url": "/static/css/main.3d6a2e01.chunk.css"
+  },
+  {
+    "revision": "32c45efa0ccdbb0b2a0c",
+    "url": "/static/js/2.9c0abff5.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.9c0abff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d3e34361a1d5610b24",
+    "url": "/static/js/3.5dba6050.chunk.js"
+  },
+  {
+    "revision": "283140cf90b87553dbe0",
+    "url": "/static/js/main.7e5739b0.chunk.js"
+  },
+  {
+    "revision": "da449b02af33ff0cfdb3",
+    "url": "/static/js/runtime-main.5ba4ebae.js"
+  },
+  {
+    "revision": "00397b42275e8fba8bec2f8d8ab982f7",
+    "url": "/static/media/cross.00397b42.jpg"
+  },
+  {
+    "revision": "e49c4b6b7cb6ad9f2e13cb5e075e5032",
+    "url": "/static/media/ping.e49c4b6b.mp3"
+  }
+]);
